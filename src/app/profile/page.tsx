555af72170a5cde3742a79d043c929399a6864aa
techmannih <manishchaudharyttt@gmail.com>
2023-12-29 13:24:58 +0530

    add logout functionality

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,35 @@
+"use client";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage() {
+    const router = useRouter();
+
+    const logout = async () => {
+        console.log("logout processing")
+        try {
+            await axios.get('/api/users/logout')
+            toast.success('Logged out successfully!')
+            router.push('/login')
+        } catch (error: any) {
+            console.log(error.message);
+            toast.error(error.message);
+        }
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-6xl">Profile</h1>
-            <hr />
-            <p className="text-xl my-2">Profie Page</p>
 
+            <h1>Profile</h1>
+            <hr />
+            <p>Profile Page</p>
+            <hr />
+            <button
+                onClick={logout}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4">
+                Logout
+            </button>
         </div>
     )
 }
