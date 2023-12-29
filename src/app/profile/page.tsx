9cd50d7fcfafee0a4a3305040efd21a81e8c4bb8
techmannih <manishchaudharyttt@gmail.com>
2023-12-29 11:55:24 +0530

    add profile

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+export default function ProfilePage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="text-6xl">Profile</h1>
+            <hr />
+            <p className="text-xl my-2">Profie Page</p>
+
+        </div>
+    )
+}
